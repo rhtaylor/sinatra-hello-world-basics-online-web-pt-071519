3aa14f552c56040205e354a1866f544641c34660
Randy Taylor <randyisbusy@gmail.com>
2019-10-03 16:44:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,9 +3,7 @@ class App < Sinatra::Base
   # Define your GET '/' route below and respond with "Hello, World!"
 get "/" do
   <<-YOO
-  Well that is when it started
-  The beginning of the end of life being eh
-  and adding art to it
+  Hello, World!
   YOO
 end
 end
